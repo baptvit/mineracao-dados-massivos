8ff40b52ecacbf54e82d49d89269fe3bda0a80de
baptvit <jvrbaptista@live.com>
2024-06-05 09:44:54 -0300

    feat: spike using hudi format

---
+++ apps/tests/hudi/__init__.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+import os
+
+# SUBMIT_ARGS = "--spark.jars /opt/spark/jars/hudi-spark3.4-bundle_2.12-0.14.1.jar pyspark"
+# os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+
+conf = (
+    SparkConf()
+    .setMaster("local[*]")
+    .setAppName("Hudi Spark Application")
+    .set("spark.jars", "/opt/spark/jars/hudi-spark3.4-bundle_2.12-0.14.1.jar")
+    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .set(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.hudi.catalog.HoodieCatalog",
+    )
+    .set(
+        "spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension"
+    )
+    .set("spark.kryo.registrator", "org.apache.spark.HoodieSparkKryoRegistrar")
+    .set("spark.sql.hive.convertMetastoreParquet", "false")
+)
+SPARK_HUDI = SparkContext(conf=conf).getOrCreate()
+
+# SPARK_HUDI = SparkSession.builder \
+#     .master("local") \
+#     .appName("Hudi Spark Application") \
+#     .config("spark.packages", "org.apache.hudi:hudi-spark3.4-bundle_2.12-0.14.1.jar") \
+#     .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+#     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog") \
+#     .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension") \
+#     .config("spark.kryo.registrator", "org.apache.spark.HoodieSparkKryoRegistrar") \
+#     .config('spark.sql.hive.convertMetastoreParquet', 'false') \
+#     .getOrCreate()
