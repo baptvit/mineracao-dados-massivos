a68cb461ff74cc7e0ca297a49239e2f3c4b09426
baptvit <jvrbaptista@live.com>
2024-06-05 09:46:58 -0300

    feat: update test spark session

--- apps/tests/__init__.py
+++ apps/tests/__init__.py
@@ -1,3 +1,3 @@
 from pyspark.sql import SparkSession
 
-SPARK = SparkSession.builder.appName("UnitTests").getOrCreate()
+SPARK = SparkSession.builder.appName("UnitTests").config("spark.driver.memory", "15g").config("spark.executor.memory", "15g").getOrCreate()
